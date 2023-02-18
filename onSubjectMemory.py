8fcfdcd304262e84120f768388e44801694098d6
Bobby Ayvazov <2066244a@student.gla.ac.uk>
2023-02-18 17:20:05 +0000

    stages 135

--- onSubjectMemory.py
+++ onSubjectMemory.py
@@ -1,4 +1,32 @@
+
+
 class Solution():
+    def stage1(nums):
+        if nums[4] == 1:
+            return 1
+        elif nums[4] == 2:
+            return 1
+        elif nums[4] == 3:
+            return 2
+        elif nums[4] == 4:
+            return 3
 
-    def __init__(self, nums):
-
+    def stage3(nums, stage1_response, stage2_response):
+        if nums[4] == 1:
+            return stage2_response
+        elif nums[4] == 2:
+            return stage1_response
+        elif nums[4] == 3:
+            return 2
+        elif nums[4] == 4:
+            return nums.index(4)
+    
+    def stage5(nums, stage1_response, stage2_response, stage3_response, stage4_response):
+        if nums[4] == 1:
+            return stage1_response
+        elif nums[4] == 2:
+            return stage2_response
+        elif nums[4] == 3:
+            return stage3_response
+        elif nums[4] == 4:
+            return stage4_response
